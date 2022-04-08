b18e15c4da4fb3c59fd45d4346e7fb63c5f37dd2
Hazem Essam <hzmessam.dev@outlook.com>
2022-04-08 23:20:19 +0200

    Add file model

--- files/models.py
+++ files/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    filename = models.CharField(max_length=100, unique=True)
+    STATUS_CHOICES = [
+        ('ENC', 'encrypted'),
+        ('DEC', 'decrypted'),
+        ('DEL', 'deleted')
+    ]
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # TODO
+    # Add owner_id field
