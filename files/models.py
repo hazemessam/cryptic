f0820f048c55ab942a7c06608ce00a6587e32aeb
Hazem Essam <hzmessam.dev@outlook.com>
2022-05-03 09:39:20 +0200

    Refactor file model

--- files/models.py
+++ files/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class File(models.Model):
-    STATUS_CHOICES = [
-        ('ENC', 'Encrypted'),
-        ('DEC', 'Decrypted'),
-        ('DEL', 'Deleted')
-    ]
+    class Status(models.TextChoices):
+        ENCRYPTED = ('ENC', 'Encrypted')
+        DECRYPTED = ('DEC', 'Decrypted')
+        DELETED = ('DEL', 'Deleted')
     
     filename = models.CharField(max_length=100, unique=True)
-    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    status = models.CharField(max_length=3, choices=Status.choices,
+                              default=Status.ENCRYPTED)
     updated_at = models.DateTimeField(auto_now=True)
 
     # TODO: Add owner field
