e9411a25142b8dbc591818c4a5f0ecc58ef3f6f4
Hazem Essam <hzmessam.dev@outlook.com>
2022-05-03 09:38:29 +0200

    Use django AbstractUser model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True, null=False, blank=False)
+class User(AbstractUser):
+    email = None
     name = models.CharField(max_length=50, null=False, blank=False)
-    password = models.CharField(max_length=50, null=False, blank=False)
-    
